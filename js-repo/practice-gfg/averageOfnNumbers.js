c3a07ca8324017c12bb56cd154aec8ffa6805947
Sayan Moulick <sayanmoulick@gmail.com>
2023-07-25 15:13:15 +0530

    branching

--- js-repo/practice-gfg/averageOfnNumbers.js
+++
@@ -1,1 +0,0 @@
-// print factors also
--- js-repo/practice-gfg/sum.js
+++
@@ -1,9 +0,0 @@
-// add two numbers using recursion
-
-// Check Whether a Number can be Expressed as Sum of Two Prime Numbers
-
-// Sum of Natural Numbers Using Recursion
-function addNumbers (n) {
-  if (n != 0) return n + addNumbers(n - 1);
-  else return n;
-}
